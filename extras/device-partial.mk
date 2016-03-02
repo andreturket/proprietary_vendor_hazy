7170a3d81bb5b2bb24f75d761c530db7cc9b5c8e
Salvatore Festa <salvatorefestatech@gmail.com>
2016-03-02 17:58:39 +0100

    Revert "Add SuperSU 2.52 support"

--- extras/device-partial.mk
+++ extras/device-partial.mk
@@ -1,5 +1,3 @@
 # Extra Packages and Files
 PRODUCT_COPY_FILES += \
-    vendor/hazy/extras/etc/init.d/99fstrim:system/etc/init.d/99fstrim:extras \
-    vendor/hazy/extras/supersu/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
-    vendor/hazy/extras/supersu/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+    vendor/hazy/extras/etc/init.d/99fstrim:system/etc/init.d/99fstrim:extras
