86f165223d57224b06b52576d75d0f3d41346ba3
Salvatore Festa <salvatorefestatech@gmail.com>
2016-04-14 09:03:27 +0200

    bullhead: Updated fingerprint to MHC19Q

--- products/hazy_bullhead.mk
+++ products/hazy_bullhead.mk
@@ -28,5 +28,5 @@ PRODUCT_MANUFACTURER := LGE
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MMB29Q/2480792:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MMB29Q 2480792 release-keys"
+    BUILD_FINGERPRINT="google/bullhead/bullhead:6.0.1/MHC19Q/2705526:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MHC19Q 2705526 release-keys"
