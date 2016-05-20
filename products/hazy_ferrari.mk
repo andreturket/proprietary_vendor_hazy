9574c4bf51d13fd0d7255f10ca559dec0fb657dd
Salvatore Festa <valoxblood@gmail.com>
2016-05-20 12:02:24 +0200

    ferrari: Added support

---
+++ products/hazy_ferrari.mk
@@ -0,0 +1,34 @@
+# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2016 The Hazy Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/xiaomi/ferrari/full_ferrari.mk)
+
+$(call inherit-product, vendor/hazy/main.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := ferrari
+PRODUCT_NAME := hazy_ferrari
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 4i
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Build prop overrides
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="ferrari" \
+    PRODUCT_NAME="ferrari" \
+    BUILD_FINGERPRINT="Xiaomi/ferrari/ferrari:6.0.1/MMB29M:userdebug/test-keys" \
+    PRIVATE_BUILD_DESC="ferrari-userdebug 6.0.1 MMB29M test-keys"
