0f22c3ca632bc8f7e814efe61ef2c02a446deb00
Andrea Turchet (aka rap_turkez) <andre.turky@gmail.com>
2016-05-25 19:04:16 +0200

    Update AndroidProducts.mk

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -2,4 +2,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/hazy_shamu.mk \
     $(LOCAL_DIR)/hazy_flounder.mk \
     $(LOCAL_DIR)/hazy_angler.mk \
-    $(LOCAL_DIR)/hazy_Z00A.mk
+    $(LOCAL_DIR)/hazy_Z00A.mk \
+    $(LOCAL_DIR)/hazy_bacon.mk
