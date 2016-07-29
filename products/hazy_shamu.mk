f13f97133e745e10562b2d241c448d51805f15ea
Salvatore Festa <salvatorefestatech@gmail.com>
2016-07-29 11:34:24 +0200

    shamu: Updated fingerprint to MOB30O

--- products/hazy_shamu.mk
+++ products/hazy_shamu.mk
@@ -13,8 +13,7 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6
 PRODUCT_MANUFACTURER := motorola
 
-# Device Fingerprint
+# Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MOB30I/2756745:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30I 2756745 release-keys"
+	BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MOB30O/2920157:user/release-keys \
+	PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30O 2920157 release-keys"
